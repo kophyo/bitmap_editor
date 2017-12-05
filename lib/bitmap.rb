382f1293a9af38faf286f1fd63852676dcd969f6
Phyo Wai Win <phyo@phyoda.co.uk>
2017-12-05 17:36:37 +0000

    Add validation for Bitmap class

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,20 +1,26 @@
 class Bitmap
-
   DEFAULT_COLOR = 'O'.freeze
+
+  MAX_WIDTH = MAX_HEIGHT = 250
+  MIN_WIDTH = MIN_HEIGHT = 1
   attr_reader :image
+
   def initialize(width:, height:, color: DEFAULT_COLOR)
     @width, @height, @color = width.to_i, height.to_i, color
+    validate
     initialize_pixels(@color)
   end
 
   def color_pixel(x:, y:, color:)
     x, y = x.to_i, y.to_i
-    
+
+    return unless valid_coordinates?(x, y)
     @image[y-1][x-1] = color
   end
 
   def draw_vertical_line(x:, from_y:, to_y:, color:)
     x, from_y, to_y = x.to_i, from_y.to_i, to_y.to_i
+    return unless valid_vertical_line?(x, from_y, to_y)
 
     (from_y..to_y).each do |y|
       color_pixel(x: x, y: y, color: color)
@@ -23,6 +29,7 @@ class Bitmap
 
   def draw_horizontal_line(from_x:, to_x:, y:, color:)
     from_x, to_x, y = from_x.to_i, to_x.to_i, y.to_i
+    return unless valid_horizontal_line?(from_x, to_x, y)
 
     (from_x..to_x).each do |x|
       color_pixel(x: x, y: y, color: color)
@@ -39,7 +46,36 @@ class Bitmap
 
   private
 
+  def valid_vertical_line?(x, from_y, to_y)
+    valid_x?(x) && valid_y?(from_y) && valid_y?(to_y) && from_y <= to_y
+  end
+
+  def valid_horizontal_line?(from_x, to_x, y)
+    valid_x?(from_x) && valid_x?(to_x) && valid_y?(y) && from_x <= to_x
+  end
+
+  def valid_coordinates?(x, y)
+    valid_x?(x) && valid_y?(y)
+  end
+  
+  def valid_x?(x)
+    x.between?(MIN_WIDTH, @width)
+  end
+
+  def valid_y?(y)
+    y.between?(MIN_HEIGHT, @height)
+  end
+
   def initialize_pixels(color)
     @image = Array.new(@height) { Array.new(@width, color)}
+  end
+
+  def validate
+    raise 'Invalid width & height provided. Must be between 1..250' unless valid_dimension?
+  end
+
+  def valid_dimension?
+    @width.between?(MIN_WIDTH, MAX_WIDTH) &&
+      @height.between?(MIN_HEIGHT, MAX_HEIGHT)
   end
 end
--- spec/unit/bitmap_spec.rb
+++ spec/unit/bitmap_spec.rb
@@ -38,6 +38,44 @@ RSpec.describe Bitmap do
         expect(subject.image).to eq(expected_image)
       end
     end
+
+    describe 'validation' do
+      context 'width' do
+        context 'less than min' do
+          let(:width) { -1 }
+
+          it 'raise error' do
+            expect{ subject }.to raise_error('Invalid width & height provided. Must be between 1..250')
+          end
+        end
+
+        context 'more than max' do
+          let(:width) { 251}
+
+          it 'raise error' do
+            expect{ subject }.to raise_error('Invalid width & height provided. Must be between 1..250')
+          end
+        end
+      end
+
+      context 'height' do
+        context 'less than min' do
+          let(:height) { -1 }
+
+          it 'raise error' do
+            expect{ subject }.to raise_error('Invalid width & height provided. Must be between 1..250')
+          end
+        end
+
+        context 'more than max' do
+          let(:height) { 251}
+
+          it 'raise error' do
+            expect{ subject }.to raise_error('Invalid width & height provided. Must be between 1..250')
+          end
+        end
+      end
+    end
   end
 
   describe '#color_pixel' do
@@ -53,6 +91,22 @@ RSpec.describe Bitmap do
     it 'colors pixel' do
       subject.color_pixel(x: '3', y: '4', color: 'X')
       expect(subject.image).to eq(expected_image)
+    end
+
+    context 'with out of bounds co ordinates' do
+      let(:expected_image) do
+        [
+          %w(O O O),
+          %w(O O O),
+          %w(O O O),
+          %w(O O O)
+        ]
+      end
+
+      it 'does nothing' do
+        subject.color_pixel(x: '-1', y: '8', color: 'X')
+        expect(subject.image).to eq(expected_image)
+      end
     end
   end
 
@@ -70,6 +124,27 @@ RSpec.describe Bitmap do
       subject.draw_vertical_line(x: '2', from_y: '1', to_y: '3', color: 'X')
       expect(subject.image).to eq(expected_image)
     end
+
+    context 'with invalid line' do
+      let(:expected_image) do
+        [
+          %w(O O O),
+          %w(O O O),
+          %w(O O O),
+          %w(O O O)
+        ]
+      end
+
+      it 'does nothing for out of bounds co ords' do
+        subject.draw_vertical_line(x: '2', from_y: '1', to_y: '8', color: 'X')
+        expect(subject.image).to eq(expected_image)
+      end
+
+      it 'does nothing for invalid range' do
+        subject.draw_vertical_line(x: '2', from_y: '3', to_y: '1', color: 'X')
+        expect(subject.image).to eq(expected_image)
+      end
+    end
   end
 
   describe '#draw_horizontal_line' do
@@ -85,6 +160,27 @@ RSpec.describe Bitmap do
     it 'draws horizontal line' do
       subject.draw_horizontal_line(from_x: '1', to_x: '3', y: '2', color: 'X')
       expect(subject.image).to eq(expected_image)
+    end
+
+    context 'with invalid line' do
+      let(:expected_image) do
+        [
+          %w(O O O),
+          %w(O O O),
+          %w(O O O),
+          %w(O O O)
+        ]
+      end
+
+      it 'does nothing for out of bounds co ords' do
+        subject.draw_horizontal_line(from_x: '1', to_x: '8', y: '2', color: 'X')
+        expect(subject.image).to eq(expected_image)
+      end
+
+      it 'does nothing for invalid range' do
+        subject.draw_horizontal_line(from_x: '3', to_x: '1', y: '2', color: 'X')
+        expect(subject.image).to eq(expected_image)
+      end
     end
   end
 
