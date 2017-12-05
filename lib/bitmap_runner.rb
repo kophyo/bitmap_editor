66d357443ae67ba80023b5586332c79b730348ab
Phyo Wai Win <phyo@phyoda.co.uk>
2017-12-05 21:09:49 +0000

    Remove errors file. Refactor BitmapRunner

--- lib/bitmap_runner.rb
+++ lib/bitmap_runner.rb
@@ -18,20 +18,36 @@ class BitmapRunner
     when /I (\d+) (\d+)/
       @bitmap = Bitmap.new(width: $1, height: $2)
     when /L (\d+) (\d+) ([A-Z])/
-      bitmap ? bitmap.color_pixel(x: $1, y: $2, color: $3) : no_bitmap_error(command)
+      bitmap ? color_pixel($1, $2, $3) : no_bitmap_error(command)
     when 'C'
       bitmap ? bitmap.clear_image : no_bitmap_error(command)
     when /V (\d+) (\d+) (\d+) ([A-Z])/
-      bitmap ? bitmap.draw_vertical_line(x: $1, from_y: $2, to_y: $3, color: $4) : no_bitmap_error(command)
+      bitmap ? draw_vertical_line($1, $2, $3, $4) : no_bitmap_error(command)
     when /H (\d+) (\d+) (\d+) ([A-Z])/
-      bitmap ? bitmap.draw_horizontal_line(from_x: $1, to_x: $2, y: $3, color: $4) : no_bitmap_error(command)
+      bitmap ? draw_horizontal_line($1, $2, $3, $4) : no_bitmap_error(command)
     when 'S'
-      bitmap ? (puts bitmap.draw_image) : no_bitmap_error(command)
+      bitmap ? draw_image : no_bitmap_error(command)
     else
       puts "Invalid Command: '#{command}'. Command discarded."
     end
   end
 
+  def color_pixel(x, y, color)
+    bitmap.color_pixel(x: x, y: y, color: color)
+  end
+
+  def draw_vertical_line(x, from_y, to_y, color)
+    bitmap.draw_vertical_line(x: x, from_y: from_y, to_y: to_y, color: color)
+  end
+
+  def draw_horizontal_line(from_x, to_x, y, color)
+    bitmap.draw_horizontal_line(from_x: from_x, to_x: to_x, y: y, color: color)
+  end
+
+  def draw_image
+    puts bitmap.draw_image
+  end
+  
   def check_bitmap(cmd)
     if bitmap.nil?
       no_bitmap_error(cmd) and return false
